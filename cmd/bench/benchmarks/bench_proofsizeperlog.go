c54d79f6db7c6256823c48ddc71c41c49a83896e
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2019-02-26 17:41:25 +0100

    Finalized benchmark for proof size per log

--- cmd/bench/benchmarks/bench_proofsizeperlog.go
+++ cmd/bench/benchmarks/bench_proofsizeperlog.go
@@ -12,9 +12,9 @@ import (
 
 const (
 	PROOFSIZEPERLOG_TOTALLOGS   = 10000000
-	PROOFSIZEPERLOG_INCREMENTS  = 10
+	PROOFSIZEPERLOG_INCREMENTS  = 100
 	PROOFSIZEPERLOG_MAXLOGCOUNT = 10000
-	PROOFSIZEPERLOG_SAMPLELIMIT = 1000
+	PROOFSIZEPERLOG_SAMPLELIMIT = 50
 )
 
 // RunProofSizePerLogBench will add 10M logs and
@@ -34,7 +34,7 @@ func RunProofSizePerLogBench() {
 	graph.Write([]byte("\\begin{tikzpicture}\n\t\\begin{axis}[\n"))
 	graph.Write([]byte("\t\txlabel=Logs included in proof,\n\t\tylabel=Proof size per log (bytes)]\n"))
 	graph.Write([]byte("\n\t\t\\addplot[color=red,mark=x] coordinates {\n"))
-	graph.Write([]byte("\t\t\t(0,0)\n"))
+	//graph.Write([]byte("\t\t\t(0,0)\n"))
 	defer graph.Close()
 
 	// Store the log IDs into one big byteslice
@@ -89,75 +89,54 @@ func RunProofSizePerLogBench() {
 
 	fmt.Printf("\rRunning proof size per log benchmark: [Committed the log, generating proofs]                  ")
 
+	loops := PROOFSIZEPERLOG_MAXLOGCOUNT / PROOFSIZEPERLOG_INCREMENTS
+
 	// Make an arrays for keeping the total size of received
 	// proofs, one element per proofLogs element
-	receivedProofs := make([]int64, PROOFSIZEPERLOG_MAXLOGCOUNT/PROOFSIZEPERLOG_INCREMENTS)
-	receivedProofSizes := make([]int64, PROOFSIZEPERLOG_MAXLOGCOUNT/PROOFSIZEPERLOG_INCREMENTS)
+	var receivedProofs, receivedProofSizes int64
 
-	wg = sync.WaitGroup{}
-	// Get proof sizes for all requested # logs by getting all possible
-	// samples from the full tree
-	for i := 0; i < PROOFSIZEPERLOG_TOTALLOGS; i++ {
-		wg.Add(1)
-		go func(pIdx int) {
-			// Take random logIDs from the known logIDs the size of the
-			// desired number of proofs
-			logIdSets := make([][][]byte, len(receivedProofs))
-			maxProofLogs := 0
-			for idx := range receivedProofs {
-				pl := ((idx + 1) * PROOFSIZEPERLOG_INCREMENTS)
-				numLogs := atomic.LoadInt64(&receivedProofs[idx])
-				// Only try the slice if it still fits within the bounds of our logs collection
-				// Don't use overlapping slices and cap it on ~PROOFSIZEPERLOG_SAMPLELIMIT samples.
-				// Because this function is executed in parallel there will be _at least_ PROOFSIZEPERLOG_SAMPLELIMIT
-				// samples per run. Could be slightly more
-				if pIdx+pl < PROOFSIZEPERLOG_TOTALLOGS && (pIdx%pl == 0) && numLogs < PROOFSIZEPERLOG_SAMPLELIMIT {
-					logIdSets[idx] = make([][]byte, pl)
-					if pl > maxProofLogs {
-						maxProofLogs = pl
-					}
+	for idx := 0; idx < loops; idx++ {
+		fmt.Printf("\rRunning proof size per log benchmark: [Generating proofs %d / %d]                  ", idx, loops)
+
+		pl := ((idx + 1) * PROOFSIZEPERLOG_INCREMENTS)
+		logIdSets := make([][][]byte, PROOFSIZEPERLOG_SAMPLELIMIT)
+		logSetIdx := 0
+		for i := 0; i < PROOFSIZEPERLOG_TOTALLOGS; i++ {
+			if i+pl < PROOFSIZEPERLOG_TOTALLOGS && (i%pl == 0) {
+				logIdSets[logSetIdx] = make([][]byte, pl)
+				for j := i; j < i+pl; j++ {
+					offset := j * 32
+					logIdSets[logSetIdx][j-i] = logIds[offset : offset+32]
 				}
+				logSetIdx++
 			}
-
-			// Fill the LogID sets with random logIDs
-			for logId := 0; logId < PROOFSIZEPERLOG_MAXLOGCOUNT; logId++ {
-				for idx := range receivedProofs {
-					pl := ((idx + 1) * PROOFSIZEPERLOG_INCREMENTS)
-					if logId < pl && len(logIdSets[idx]) > 0 {
-						offset := (pIdx + logId) * 32
-						logIdSets[idx][logId] = logIds[offset : offset+32]
-					}
-				}
+			if logSetIdx >= PROOFSIZEPERLOG_SAMPLELIMIT {
+				break
 			}
+		}
 
-			// Get the proof for the keys in each of the sets and then
-			// calculate their size
-			var wg2 sync.WaitGroup
-			for ipL := range receivedProofs {
-				if len(logIdSets[ipL]) > 0 {
-					wg2.Add(1)
-					go func(idx int) {
-						partialMPT, _ := srv.GetProofForKeys(logIdSets[idx])
-						atomic.AddInt64(&(receivedProofs[idx]), int64(1))
-						atomic.AddInt64(&(receivedProofSizes[idx]), int64(partialMPT.ByteSize()))
-						wg2.Done()
-					}(ipL)
-				}
+		// Get the proof for the keys in each of the sets and then
+		// calculate their size
+		var wg2 sync.WaitGroup
+		for ipL := 0; ipL < loops; ipL++ {
+			if len(logIdSets[ipL]) > 0 {
+				wg2.Add(1)
+				go func(idx int) {
+					partialMPT, _ := srv.GetProofForKeys(logIdSets[idx])
+					atomic.AddInt64(&receivedProofs, int64(1))
+					atomic.AddInt64(&receivedProofSizes, int64(partialMPT.ByteSize()))
+					wg2.Done()
+				}(ipL)
 			}
+		}
 
-			wg2.Wait()
-			logIdSets = nil
-			wg.Done()
-		}(i)
-	}
+		wg2.Wait()
 
-	wg.Wait()
-
-	// Write the average sampled sizes to the TEX file
-	for idx := range receivedProofs {
-		avgProofSize := atomic.LoadInt64(&receivedProofSizes[idx]) / atomic.LoadInt64(&receivedProofs[idx])
+		avgProofSize := atomic.LoadInt64(&receivedProofSizes) / atomic.LoadInt64(&receivedProofs)
 		avgProofSizePerLog := avgProofSize / int64((idx+1)*PROOFSIZEPERLOG_INCREMENTS)
 		graph.Write([]byte(fmt.Sprintf("\t\t\t(%d,%d)\n", (idx+1)*PROOFSIZEPERLOG_INCREMENTS, avgProofSizePerLog)))
+
+		logIdSets = nil
 	}
 
 	// Write end markers to the tex file and we're done.
