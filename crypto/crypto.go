f5488f57806d5c7449e7adab244eb0f873ce3f75
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2019-02-26 13:41:06 +0100

    Revert fixed byte slice

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -4,12 +4,11 @@ import "github.com/mit-dci/go-bverify/crypto/fastsha256"
 
 // WitnessKeyAndValue commits to a key and a value using the following commitment:
 // H(key||value)
-func WitnessKeyAndValue(key, value [32]byte) [32]byte {
+func WitnessKeyAndValue(key, value []byte) []byte {
 	hasher := fastsha256.New()
-	hasher.Write(key[:])
-	hasher.Write(value[:])
-	hash := [32]byte{}
-	copy(hash[:], hasher.Sum(nil))
+	hasher.Write(key)
+	hasher.Write(value)
+	hash := hasher.Sum(nil)
 	hasher = nil
 	return hash
 }
