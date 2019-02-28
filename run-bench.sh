8b982fa8ad01263a6acd4412a9b118e5a447ee78
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2019-02-28 13:05:43 +0100

    Script for running the benchmarks easily

---
+++ run-bench.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+echo "Building benchmark Docker container"
+docker build . -f Dockerfile.bench -t bverifybench > build.log
+echo "Removing old container (if it exists)"
+set +e
+docker rm "bverifybench-$1" > /dev/null 2> /dev/null
+echo "Starting benchmark '$1'"
+docker run -it --name "bverifybench-$1" bverifybench bin/bench "-$1"
+echo "Cleaning up old output"
+set -e
+rm -rf ./tmp
+echo "Copying output"
+docker cp "bverifybench-$1":/app ./tmp
+set -e
+mkdir -p "./out"
+cp ./tmp/*.tex "./out"
+rm -rf ./tmp
+docker rm "bverifybench-$1" > /dev/null 2> /dev/null
+echo "Done"
