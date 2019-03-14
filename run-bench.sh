8d4cf6c8d59d70363c61c9baba81dbbff75a0687
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2019-03-14 15:05:43 +0100

    Added port mapping to the benchmark script for server bench

--- run-bench.sh
+++ run-bench.sh
@@ -6,7 +6,7 @@ echo "Removing old container (if it exists)"
 set +e
 docker rm "bverifybench-$1" > /dev/null 2> /dev/null
 echo "Starting benchmark '$1'"
-docker run -it --name "bverifybench-$1" bverifybench bin/bench "-$1"
+docker run -p 9100:9100 -it --name "bverifybench-$1" bverifybench bin/bench "-$1"
 echo "Cleaning up old output"
 set -e
 rm -rf ./tmp
