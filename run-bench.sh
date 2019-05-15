5db95416f1caaec7ca4a890a95bef62f49edc0ab
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2019-05-15 08:27:10 +0200

    Include profiling in benchmarks

--- run-bench.sh
+++ run-bench.sh
@@ -6,7 +6,7 @@ echo "Removing old container (if it exists)"
 set +e
 docker rm "bverifybench-$1" > /dev/null 2> /dev/null
 echo "Starting benchmark '$1'"
-docker run -p 6060:6060 -p 9100:9100 -it --name "bverifybench-$1" bverifybench bin/bench "-profile" "-$1"
+docker run -p 6060:6060 -p 9100:9100 -it --name "bverifybench-$1" bverifybench bin/bench "-profile" "-cpuprofile=cpu.pprof" "-memprofile=mem.pprof" "-$1"
 echo "Cleaning up old output"
 set -e
 rm -rf ./tmp
@@ -16,6 +16,7 @@ set -e
 mkdir -p "./out"
 cp ./tmp/*.tex "./out"
 cp ./tmp/*.raw "./out"
+cp ./tmp/*.pprof "./out"
 rm -rf ./tmp
 docker rm "bverifybench-$1" > /dev/null 2> /dev/null
 echo "Done"
